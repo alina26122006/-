ed6ceb2a224ee6830dd7e974a7b3f89fb0c655fc
лина <@DESKTOP-QOG2EQH>
2024-03-06 17:34:55 +0300

    Добавьте файлы проекта.

---
+++ кт/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace кт
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем объект класса Vector
+            Vector vector = new Vector(1, 2, 3, 4);
+
+            // Перегруженный оператор умножения на число
+            Vector result = 2.5 * vector;
+
+            // Выводим результат
+            Console.WriteLine("StartX: {0}, StartY: {1}, EndX: {2}, EndY: {3}",
+                result.StartX, result.StartY, result.EndX, result.EndY);
+        }
+    }
+}
---
+++ кт/Vector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace кт
+{
+    internal class Vector
+    {
+        public double StartX { get; set; }
+        public double StartY { get; set; }
+        public double EndX { get; set; }
+        public double EndY { get; set; }
+
+        public Vector(double startX, double startY, double endX, double endY)
+        {
+            StartX = startX;
+            StartY = startY;
+            EndX = endX;
+            EndY = endY;
+        }
+
+        public static Vector operator *(double scalar, Vector vector)
+        {
+            Vector result = new Vector(
+                scalar * vector.StartX,
+                scalar * vector.StartY,
+                scalar * vector.EndX,
+                scalar * vector.EndY
+            );
+            return result;
+        }
+    }
+}
